5fbd5ebfee49e287c9a22cf8fdf113cd11ac8dda
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-08-23 12:17:52 +0500

    Some codes changen in web

--- module-web/src/main/java/com/mjc/school/controller/impl/AuthorController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/AuthorController.java
@@ -1,59 +1,44 @@
 package com.mjc.school.controller.impl;
 
 import com.mjc.school.controller.BaseController;
-import com.mjc.school.controller.CommandHandler;
-import com.mjc.school.controller.dto.AuthorRequestDto;
-import com.mjc.school.controller.dto.AuthorResponseDto;
-import com.mjc.school.controller.mapper.ServiceToWebDTOMapper;
 import com.mjc.school.service.BaseService;
-import com.mjc.school.service.dto.ServiceAuthorRequestDto;
-import com.mjc.school.service.dto.ServiceAuthorResponseDto;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.mjc.school.service.dto.AuthorRequestDTO;
+import com.mjc.school.service.dto.AuthorResponseDTO;
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class AuthorController implements BaseController<AuthorRequestDto, AuthorResponseDto, Long> {
-    private final BaseService<ServiceAuthorRequestDto, ServiceAuthorResponseDto, Long> authorService;
-    private final ServiceToWebDTOMapper mapper = new ServiceToWebDTOMapper();
+public class AuthorController implements BaseController<AuthorRequestDTO, AuthorResponseDTO, Long> {
+    private final BaseService<AuthorRequestDTO, AuthorResponseDTO, Long> authorService;
 
-    public AuthorController(
-            @Qualifier("authorService")
-            BaseService<ServiceAuthorRequestDto, ServiceAuthorResponseDto, Long> authorService
-    ) {
+    public AuthorController(BaseService<AuthorRequestDTO, AuthorResponseDTO, Long> authorService) {
         this.authorService = authorService;
     }
 
-    @CommandHandler(code = 6)
-    public List<AuthorResponseDto> readAll() {
-        List<AuthorResponseDto> authorResponseDtoList = new ArrayList<>();
-        for (ServiceAuthorResponseDto responseDto : authorService.readAll()) {
-            authorResponseDtoList.add(mapper.mapServiceAuthorResponseDto(responseDto));
-        }
-        return authorResponseDtoList;
+
+    @Override
+    public List<AuthorResponseDTO> readAll() {
+        return authorService.readAll();
     }
 
-    @CommandHandler(code = 7)
-    public AuthorResponseDto readById(Long newsId) {
-        return mapper.mapServiceAuthorResponseDto(authorService.readById(newsId));
+    @Override
+    public AuthorResponseDTO readById(Long id) {
+        return authorService.readById(id);
     }
 
-    @CommandHandler(code = 8)
-    public AuthorResponseDto create(AuthorRequestDto dtoRequest) {
-        return mapper.mapServiceAuthorResponseDto(
-                authorService.create(mapper.mapAuthorRequestDto(dtoRequest)));
+    @Override
+    public AuthorResponseDTO create(AuthorRequestDTO createRequest) {
+        return authorService.create(createRequest);
     }
 
-    @CommandHandler(code = 9)
-    public AuthorResponseDto update(AuthorRequestDto dtoRequest) {
-        return mapper.mapServiceAuthorResponseDto(
-                authorService.update(mapper.mapAuthorRequestDto(dtoRequest)));
+    @Override
+    public AuthorResponseDTO update(AuthorRequestDTO updateRequest) {
+        return authorService.update(updateRequest);
     }
 
-    @CommandHandler(code = 10)
-    public boolean deleteById(Long newsId) {
-        return authorService.deleteById(newsId);
+    @Override
+    public boolean deleteById(Long id) {
+        return authorService.deleteById(id);
     }
 }
--- module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
@@ -1,61 +1,49 @@
 package com.mjc.school.controller.impl;
 
 import com.mjc.school.controller.BaseController;
-import com.mjc.school.controller.CommandHandler;
-import com.mjc.school.controller.dto.NewsRequestDto;
-import com.mjc.school.controller.dto.NewsResponseDto;
-import com.mjc.school.controller.mapper.ServiceToWebDTOMapper;
 import com.mjc.school.service.BaseService;
-import com.mjc.school.service.dto.ServiceNewsRequestDto;
-import com.mjc.school.service.dto.ServiceNewsResponseDto;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.mjc.school.service.dto.NewsRequestDTO;
+import com.mjc.school.service.dto.NewsResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
 @Controller
-public class NewsController implements BaseController<NewsRequestDto, NewsResponseDto, Long> {
-    private final BaseService<ServiceNewsRequestDto, ServiceNewsResponseDto, Long> newsService;
+public class NewsController implements BaseController<NewsRequestDTO, NewsResponseDTO, Long> {
+    private final BaseService<NewsRequestDTO, NewsResponseDTO, Long> newsService;
 
-    private final ServiceToWebDTOMapper mapper = new ServiceToWebDTOMapper();
-
-    public NewsController(
-            @Qualifier("newsService")
-            BaseService<ServiceNewsRequestDto, ServiceNewsResponseDto, Long> newsService
-    ) {
+    @Autowired
+    public NewsController(BaseService<NewsRequestDTO, NewsResponseDTO, Long> newsService) {
         this.newsService = newsService;
     }
 
-    @CommandHandler(code = 1)
-    public List<NewsResponseDto> readAll() {
-        List<NewsResponseDto> newsResponseDtoList = new ArrayList<>();
-        for (ServiceNewsResponseDto serviceDto : newsService.readAll()) {
-            newsResponseDtoList.add(mapper.mapServiceNewsResponseDto(serviceDto));
-        }
-        return newsResponseDtoList;
+
+    @Override
+    public List<NewsResponseDTO> readAll() {
+        return newsService.readAll();
     }
 
-    @CommandHandler(code = 2)
-    public NewsResponseDto readById(Long newsId) {
-        return mapper.mapServiceNewsResponseDto(
-                newsService.readById(newsId));
+
+    @Override
+    public NewsResponseDTO readById(Long id) {
+        return newsService.readById(id);
     }
 
-    @CommandHandler(code = 3)
-    public NewsResponseDto create(NewsRequestDto dtoRequest) {
-        return mapper.mapServiceNewsResponseDto(
-                newsService.create(mapper.mapNewsRequestDto(dtoRequest)));
+
+    @Override
+    public NewsResponseDTO create(NewsRequestDTO createRequest) {
+        return newsService.create(createRequest);
     }
 
-    @CommandHandler(code = 4)
-    public NewsResponseDto update(NewsRequestDto dtoRequest) {
-        return mapper.mapServiceNewsResponseDto(
-                newsService.update(mapper.mapNewsRequestDto(dtoRequest)));
+
+    @Override
+    public NewsResponseDTO update(NewsRequestDTO updateRequest) {
+        return newsService.update(updateRequest);
     }
 
-    @CommandHandler(code = 5)
-    public boolean deleteById(Long newsId) {
-        return newsService.deleteById(newsId);
+
+    @Override
+    public boolean deleteById(Long id) {
+        return newsService.deleteById(id);
     }
 }
